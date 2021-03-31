767feb48c24d32e310f05de8371b9bad904773ae
Celeste2298 <73576581+Celeste2298@users.noreply.github.com>
2021-03-30 23:01:21 -0700

    Initial commit

---
+++ unsolved/js/index.js
@@ -0,0 +1,14 @@
+// Create the addNumbers function here.
+function addNumbers() {
+  var two= 2;
+  var three= 3;
+  
+  var five= two + three;
+
+  console.log(five);
+
+}
+
+// Create the stringConcat function here.
+
+
